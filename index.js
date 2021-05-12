8d32ac9277ad881c5fdc3927083254895d5c1064
nixage <xodanych98@mail.ru>
2021-05-12 10:30:45 +0300

    'init'

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const events = require('events')
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const emitter = new events.EventEmitter();
 

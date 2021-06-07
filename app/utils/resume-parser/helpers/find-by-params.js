d5e356bed325cb9499cdbe3abb3fb9b01c3c690b
nixage <xodanych98@mail.ru>
2021-06-07 13:08:08 +0300

    bug fix parse name

--- app/utils/resume-parser/helpers/find-by-params.js
+++
@@ -1,7 +0,0 @@
-const re = /([\)\(\/+])/g;
-module.exports = (arr, text) => {
-  return arr
-    .map((val) => val.replace(re, '\\$1'))
-    .filter((val) => new RegExp(`\\b${val}\\b`, 'i').exec(text) !== null)
-    .join();
-};
--- app/utils/resume-parser/parser/skills-parser/skills.parser.js
+++ app/utils/resume-parser/parser/skills-parser/skills.parser.js
@@ -1,10 +1,10 @@
 const { keySkills } = require('../../dictionary/skills/key-skills.json');
 
-const findFirstIndexByTitles = require('../../helpers/find-index-by-title');
+const findLastInedPersonalInfo = require('../../helpers/find-last-index-personal-info');
 
 module.exports = (text = '') => {
   if (text.length) {
-    const index = findFirstIndexByTitles(text);
+    const index = findLastInedPersonalInfo(text);
     const newText = index !== -1 ? text.slice(index, -1) : text;
     const skillsString = keySkills
       .map((val) => `(?<![.])\\b${val}\\b`)

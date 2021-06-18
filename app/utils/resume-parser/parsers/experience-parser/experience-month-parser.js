8ed4ec55df061e4437aaf7dd0286c712e4e36e20
nixage <xodanych98@mail.ru>
2021-06-18 16:26:31 +0300

    update experience date parser

--- app/utils/resume-parser/parsers/experience-parser/experience-month-parser.js
+++ app/utils/resume-parser/parsers/experience-parser/experience-month-parser.js
@@ -1,8 +1,8 @@
 const dictionary = require('../../dictionary/dictionary');
 
 module.exports = (text) => {
-  let num = dictionary.months.find((month) => new RegExp(month.re, 'i').test(text))?.number;
-  if (!num) num = /(\d{1,2})/.exec(text)?.[0];
-  if (num?.length === 1) num = '0' + num;
-  return num || '';
+  let month = dictionary.months.find((month) => new RegExp(month.re, 'i').test(text))?.number;
+  if (!month) month = /(\d{1,2})/.exec(text)?.[0];
+  if (month?.length === 1) month = '0' + month;
+  return month || '';
 };

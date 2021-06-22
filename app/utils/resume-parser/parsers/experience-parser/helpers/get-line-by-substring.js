642e228a08fc6da9a99d6dc36e608e13dd04d835
nixage <xodanych98@mail.ru>
2021-06-22 11:00:31 +0300

    update experience parser

--- app/utils/resume-parser/parsers/experience-parser/helpers/get-line-by-substring.js
+++ app/utils/resume-parser/parsers/experience-parser/helpers/get-line-by-substring.js
@@ -1,7 +1,12 @@
 module.exports = (str, substr) => {
-  let line = new RegExp(`(.+|.?)${substr}(.+|.?)`).exec(str);
-  if ((line[1] + line[2]).length < 30) {
-    line = new RegExp(`(.|\n){10}${substr}(([.\s\n]?.+){1,2})`, 'm').exec(str);
+  let line = '';
+  const re = new RegExp(`(.+|.?)${substr}(.+|.?)`).exec(str);
+  line = re[1] + re[2];
+  if (line.length < 30) {
+    // const re = new RegExp(`((?:[.\\s\n]?.*){1,3})${substr}(.*\n)`, 'm').exec(str);
+    // const re = new RegExp(`(.*(?:(?:\\b\\w+\\b[\\s\\,\n]*){1,4}))${substr}(.*\n)`, 'm').exec(str);
+    const re = new RegExp(`(.+(?:.+[\\s\n.\\,]?){1,2})${substr}(.*\n)`, 'm').exec(str);
+    line = re[1];
   }
-  return line ? line[1] + line[2] : '';
+  return line;
 };

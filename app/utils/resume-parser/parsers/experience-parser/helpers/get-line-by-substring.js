4fc3e0f32f9ef07329fe0be350e4b311aebd21fe
nixage <xodanych98@mail.ru>
2021-06-22 11:08:15 +0300

    bug fix

--- app/utils/resume-parser/parsers/experience-parser/helpers/get-line-by-substring.js
+++ app/utils/resume-parser/parsers/experience-parser/helpers/get-line-by-substring.js
@@ -6,7 +6,7 @@ module.exports = (str, substr) => {
     // const re = new RegExp(`((?:[.\\s\n]?.*){1,3})${substr}(.*\n)`, 'm').exec(str);
     // const re = new RegExp(`(.*(?:(?:\\b\\w+\\b[\\s\\,\n]*){1,4}))${substr}(.*\n)`, 'm').exec(str);
     const re = new RegExp(`(.+(?:.+[\\s\n.\\,]?){1,2})${substr}(.*\n)`, 'm').exec(str);
-    line = re[1];
+    line = re ? re[1] : '';
   }
   return line;
 };
